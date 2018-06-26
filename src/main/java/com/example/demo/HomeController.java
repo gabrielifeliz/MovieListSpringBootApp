459006931df3ad997201962aa44e99ecf53e62c6
Gabriel I Feliz <gfeliz3@montgomerycollege.edu>
2018-06-25 23:38:56 -0400

    updates

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,29 +1,60 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
+    @Autowired
+    MovieRepository movieRepository;
 
     @RequestMapping("/")
     public String homePage() {
         return "index";
     }
-
-    @GetMapping("/addmovie")
-    public String loadFormPage(Model model)
-    {
+    
+    @GetMapping("/add")
+    public String courseForm(Model model) {
         model.addAttribute("movie", new Movie());
         return "movieform";
     }
-
-    @PostMapping("/addmovie")
-    public String saveForm(@ModelAttribute("movie") Movie movie, Model model)
-    {
-        model.addAttribute("movie", movie);
-        return "confirmmovie";
+    
+    @PostMapping("/process")
+    public String processForm(@Valid Movie movie, BindingResult result) {
+        if (result.hasErrors()) {
+            return "movieform";
+        }
+        movieRepository.save(movie);
+        return "redirect:/";
     }
 
+    @GetMapping("/movielist")
+    public String movieList(Model model){
+        model.addAttribute("movies", movieRepository.findAll());
+        return "movielist";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showMovie(@PathVariable("id") long id, Model model) {
+        model.addAttribute("movie", movieRepository.findById(id).get());
+        return "showmovie";
+    }
+    @RequestMapping("/update/{id}")
+    public String updateMovie(@PathVariable("id") long id, Model model) {
+        model.addAttribute("movie", movieRepository.findById(id));
+        return "movieform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String deleteMovie(@PathVariable("id") long id) {
+        movieRepository.deleteById(id);
+        return "redirect:/";
+    }
 }
--- src/main/java/com/example/demo/Movie.java
+++ src/main/java/com/example/demo/Movie.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Movie {
@@ -11,9 +14,21 @@ public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @NotEmpty
+    @Size(min = 1, max = 50)
     private String director;
+
+    @NotEmpty
+    @Size(min = 1, max = 50)
     private String title;
+
+    @NotEmpty
+    @Size(min = 1, max = 140)
     private String plot;
+
+    @NotNull
+    @Size(min = 4)
     private int releaseYear;
 
     public long getId() {
